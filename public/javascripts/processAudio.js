9bd2cb674d3ef43d54a08905d32b9944e4954780
Mullenratte <Max.Blanc@web.de>
2024-12-03 08:32:10 +0100

    Hochschule

--- public/javascripts/processAudio.js
+++ public/javascripts/processAudio.js
@@ -1,25 +1,37 @@
 let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 let audioElement = document.getElementById('audio');
-//let analyser = audioCtx.createAnalyser();
+let analyser = audioCtx.createAnalyser();
 let track = audioCtx.createMediaElementSource(audioElement);
 let panSlider = document.getElementById('panSlider');
 let panning = panSlider.value;
-let stereoPanner = new StereoPannerNode(audioCtx, {pan : 0}); 
+let stereoPanner = audioCtx.createStereoPanner(); 
 
-track = track.connect(stereoPanner).connect(audioCtx.destination);
+track = track.connect(stereoPanner).connect(analyser).connect(audioCtx.destination);
 
-document.getElementById('panSlider').addEventListener('change', () =>{
-    stereoPanner.pan.value = document.getElementById('panSlider').value;
-    console.log(stereoPanner.pan.value)
-})
+let dataArray = new Uint8Array(analyser.frequencyBinCount);
+
+document.getElementById('analyse').addEventListener('click', ()=>{
+    analyser.getByteFrequencyData(dataArray);
+    AnimateData(dataArray);
+    console.log(dataArray);
+});
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
 
 document.getElementById('debug').addEventListener('click', () =>{
     console.log(stereoPanner.pan.value);
-})
+});
 
-// function UpdatePanning(){
-//     panning = document.getElementById('panSlider').value;
-//     stereoPanner.pan.value = panning;
-// }
+function UpdatePanning(){
+    stereoPanner.pan.value = document.getElementById('panSlider').value;
+}
+
+function AnimateData(dataArray){
+    for (let i = 0; i < analyser.frequencyBinCount; i++){
+        ctx.fillRect(i * 10, canvas.height - 25, 1, canvas.height - dataArray[i]);
+    }
+    req = requestAnimationFrame(AnimateData);
+}
 
 
