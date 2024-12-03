8b9b3682482b53d5103eb12159583e45ccecf210
Mullenratte <Max.Blanc@web.de>
2024-12-03 15:50:49 +0100

    implemented simple animation

--- public/javascripts/processAudio.js
+++ public/javascripts/processAudio.js
@@ -8,16 +8,18 @@ let stereoPanner = audioCtx.createStereoPanner();
 
 track = track.connect(stereoPanner).connect(analyser).connect(audioCtx.destination);
 
-let dataArray = new Uint8Array(analyser.frequencyBinCount);
 
 document.getElementById('analyse').addEventListener('click', ()=>{
-    analyser.getByteFrequencyData(dataArray);
-    AnimateData(dataArray);
-    console.log(dataArray);
+    AnimateData();
 });
 
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+document.getElementById('toggle').addEventListener('click', () =>{
+    if (audioCtx.state == 'running'){
+        audioCtx.suspend();
+    } else if (audioCtx.state == 'suspended'){
+        audioCtx.resume();
+    }
+})
 
 document.getElementById('debug').addEventListener('click', () =>{
     console.log(stereoPanner.pan.value);
@@ -27,11 +29,30 @@ function UpdatePanning(){
     stereoPanner.pan.value = document.getElementById('panSlider').value;
 }
 
-function AnimateData(dataArray){
-    for (let i = 0; i < analyser.frequencyBinCount; i++){
-        ctx.fillRect(i * 10, canvas.height - 25, 1, canvas.height - dataArray[i]);
+function AnimateData(){
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    const colWidth = 3;
+    const spaceBetweenCol = 5;
+
+
+    let dataArray = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(dataArray);
+
+    canvas.width = dataArray.length * colWidth + spaceBetweenCol; 
+    console.log(canvas.width)
+
+    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgba(254, 0, 0, 1)';
+
+    for (let i = 0; i < dataArray.length; i++){
+        ctx.fillRect(i * colWidth + spaceBetweenCol, canvas.height, colWidth, (-dataArray[i]/255) * canvas.height);
     }
-    req = requestAnimationFrame(AnimateData);
+
+    requestAnimationFrame(AnimateData);
+    
 }
 
 
